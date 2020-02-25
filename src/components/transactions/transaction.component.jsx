a1df1d419b125a49a77bb36fe5dfdb6b53015b47
hbonilla96 <hbonillab@ucenfotec.ac.cr>
2020-02-25 15:53:34 -0600

    wizard fixes

--- src/components/transactions/transaction.component.jsx
+++ src/components/transactions/transaction.component.jsx
@@ -10,41 +10,103 @@ export default function Transaction() {
   const { accounts } = useAccounts();
 
   const Step1 = () => (
-    <div>
-      {" "}
-      <div className="container register-container">
-        <div className="card main-card">
-          <div className="card-body">
-            <hr></hr>
-            <div>
-              <div>
-                <span>Destination account data</span>
-              </div>
-              <label>Destination account</label>
-              <Input
-                inputRef={raw({
-                  name: "userId",
-                  onChange: e => e.target.value,
-                  validate: (value, values, event) => {
-                    if (value === "") {
-                      return "This field is required";
-                    }
-                  }
-                })}
-                isValid={formState.validity.userId}
-                errorMessage={formState.errors.userId}
-                type={"text"}
-              />
-              <InputSelect>
-                <option value="CR">Colon </option>
-                <option value="DOL">Dolar </option>
-              </InputSelect>
-            </div>
+    <div className="container destination-container">
+      <div className="card main-card">
+        <div className="card-body">
+          <div>
+            {" "}
+            <span>Origin account data </span>
+          </div>
+          <label>Origin account</label>
+          <InputSelect>
+            {accounts &&
+              accounts.map(account => {
+                return (
+                  <option
+                    key={account.id}
+                  >{`${account.accountNumber} ${account.accountHolder} ${account.balance}`}</option>
+                );
+              })}
+          </InputSelect>
+        </div>
+      </div>
+    </div>
+  );
 
-            <button onClick={getAccounts}>accounts</button>
+  const Step2 = () => (
+    <div className="container destination-container">
+      <div className="card main-card">
+        <div className="card-body">
+          <div>
+            <div>
+              <span>Destination account data</span>
+            </div>
+            <label>Id number</label>
+            <Input
+              inputRef={raw({
+                name: "userId",
+                onChange: e => e.target.value,
+                validate: (value, values, event) => {
+                  if (value === "") {
+                    return "This field is required";
+                  }
+                }
+              })}
+              isValid={formState.validity.userId}
+              errorMessage={formState.errors.userId}
+              type={"text"}
+            />
+            <label>Destination account</label>
+            <Input
+              inputRef={raw({
+                name: "destinationAccount",
+                onChange: e => e.target.value,
+                validate: (value, values, event) => {
+                  if (value === "") {
+                    return "This field is required";
+                  }
+                }
+              })}
+              isValid={formState.validity.destinationAccount}
+              errorMessage={formState.errors.destinationAccount}
+              type={"text"}
+            />
           </div>
         </div>
       </div>
+    </div>
+  );
+
+  const Step3 = () => (
+    <div>
+      <h1>Confirmation</h1>
+    </div>
+  );
+
+  const MyStepTracker = ({ currentStep = 0, steps = [] }) => (
+    <div>
+      <p>Current step is: {steps[currentStep]}</p>
+    </div>
+  );
+  const MyNavigator = ({
+    getFirstStepProps,
+    getLastStepProps,
+    getNextStepProps,
+    getPrevStepProps
+  }) => (
+    <div>
+      <button type="button" {...getFirstStepProps()}>
+        &lt;&lt; First step
+      </button>
+      <button type="button" {...getPrevStepProps()}>
+        &lt; Go back
+      </button>
+      <button type="button" {...getNextStepProps()}>
+        Next &gt;
+      </button>
+      <button type="button" {...getLastStepProps()}>
+        Last step&gt;&gt;
+      </button>
     </div>
   );
 
@@ -53,26 +115,25 @@ export default function Transaction() {
     console.log(accounts);
   }
 
-  function onClick() {
-    this.setState(prevState => ({
-      isCustomizeVisible: !prevState.isCustomizeVisible
-    }));
+  function handleStepChange(currentStep) {
+    console.log(currentStep);
   }
 
   return (
-    <div>
-      <div>
-        <p>Step 3 visible: </p>
-        <button type="button" onClick={onClick}>
-          Toggle Step 3
-        </button>
-      </div>
-      <Wizard>
+    <div className="container steps-container">
+      <Wizard onStepChange={handleStepChange}>
         <Wizard.StepTracker />
         <Wizard.Steps>
-          <Step1 stepLabel="Search" />
-          <Wizard.StepGroup stepLabel="Submit"></Wizard.StepGroup>
+          <Step1 stepLabel="Destination account" />
+          <Step2 stepLabel="Origin account" />
+          <Step3 stepLabel="Confirmation"></Step3>
         </Wizard.Steps>
+        <Wizard.StepTracker>
+          {stepTrackerProps => <MyStepTracker {...stepTrackerProps} />}
+        </Wizard.StepTracker>
+        <Wizard.Navigator>
+          {navigatorProps => <MyNavigator {...navigatorProps} />}
+        </Wizard.Navigator>
       </Wizard>
     </div>
   );
