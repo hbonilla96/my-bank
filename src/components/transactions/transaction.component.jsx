e72a8bf566f592278251c25d8313da113f853254
hbonilla96 <hbonillab@ucenfotec.ac.cr>
2020-02-25 14:52:59 -0600

    wizard

--- src/components/transactions/transaction.component.jsx
+++ src/components/transactions/transaction.component.jsx
@@ -1,67 +1,79 @@
 import React from "react";
+import Wizard from "react-simple-step-wizard";
 import InputSelect from "../input-select/input-select.component";
+import Input from "../input/input.component";
 import { useAccounts } from "../../hooks/useAccounts";
 import { useFormState } from "react-use-form-state";
-import Input from "../input/input.component";
 
 export default function Transaction() {
   const [formState, { raw }] = useFormState();
   const { accounts } = useAccounts();
+
+  const Step1 = () => (
+    <div>
+      {" "}
+      <div className="container register-container">
+        <div className="card main-card">
+          <div className="card-body">
+            <hr></hr>
+            <div>
+              <div>
+                <span>Destination account data</span>
+              </div>
+              <label>Destination account</label>
+              <Input
+                inputRef={raw({
+                  name: "userId",
+                  onChange: e => e.target.value,
+                  validate: (value, values, event) => {
+                    if (value === "") {
+                      return "This field is required";
+                    }
+                  }
+                })}
+                isValid={formState.validity.userId}
+                errorMessage={formState.errors.userId}
+                type={"text"}
+              />
+              <InputSelect>
+                <option value="CR">Colon </option>
+                <option value="DOL">Dolar </option>
+              </InputSelect>
+            </div>
+
+            <button onClick={getAccounts}>accounts</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 
   function getAccounts(event) {
     event.preventDefault();
     console.log(accounts);
   }
 
+  function onClick() {
+    this.setState(prevState => ({
+      isCustomizeVisible: !prevState.isCustomizeVisible
+    }));
+  }
+
   return (
-    <div className="container register-container">
-      <div className="card main-card">
-        <div className="card-body">
-          <div>
-            <div>
-              {" "}
-              <span>Origin account data </span>
-            </div>
-            <label>Origin account</label>
-            <InputSelect>
-              {accounts &&
-                accounts.map(account => {
-                  return (
-                    <option>{`${account.accountNumber} ${account.accountHolder} ${account.balance}`}</option>
-                  );
-                })}
-            </InputSelect>
-          </div>
-
-          <hr></hr>
-          <div>
-            <div>
-              <span>Destination account data</span>
-            </div>
-            <label>Destination account</label>
-            <Input
-              inputRef={raw({
-                name: "userId",
-                onChange: e => e.target.value,
-                validate: (value, values, event) => {
-                  if (value === "") {
-                    return "This field is required";
-                  }
-                }
-              })}
-              isValid={formState.validity.userId}
-              errorMessage={formState.errors.userId}
-              type={"text"}
-            />
-            <InputSelect>
-              <option value="CR">Colon </option>
-              <option value="DOL">Dolar </option>
-            </InputSelect>
-          </div>
-
-          <button onClick={getAccounts}>accounts</button>
-        </div>
+    <div>
+      <div>
+        <p>Step 3 visible: </p>
+        <button type="button" onClick={onClick}>
+          Toggle Step 3
+        </button>
       </div>
+      <Wizard>
+        <Wizard.StepTracker />
+        <Wizard.Steps>
+          <Step1 stepLabel="Search" />
+          <Wizard.StepGroup stepLabel="Submit"></Wizard.StepGroup>
+        </Wizard.Steps>
+      </Wizard>
     </div>
   );
 }
