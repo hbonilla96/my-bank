70cecb48bc30afd7013856c93ea9426d801aadfe
hbonilla96 <hbonillab@ucenfotec.ac.cr>
2020-02-28 09:12:14 -0600

    heroku

--- src/services/expensesService.jsx
+++ src/services/expensesService.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getExpenses = () => {
   let userName = sessionStorage.getItem("username");
-  const expenses = `http://localhost:3378/api/v1/history/expenses/${userName}`;
+  const expenses = `https://agnatebankapi.herokuapp.com/api/v1/history/expenses/${userName}`;
   let token = sessionStorage.getItem("token");
 
   return axios.get(expenses, {
