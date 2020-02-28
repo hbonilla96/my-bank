b8c98f06037b7f648b23d60dea08154f61797e9c
hbonilla96 <hbonillab@ucenfotec.ac.cr>
2020-02-28 09:13:03 -0600

    heroku

--- src/services/transactionService.jsx
+++ src/services/transactionService.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getUserTransactions = () => {
   let token = sessionStorage.getItem("token");
   let userId = sessionStorage.getItem("username");
-  const url = `http://localhost:3378/api/v1/history/${userId}`;
+  const url = `https://agnatebankapi.herokuapp.com/api/v1/history/${userId}`;
   return axios.get(url, {
     headers: { Authorization: "Bearer " + token }
   });
