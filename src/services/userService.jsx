813a25cc38d1bef5d3ea2a1a43d6f70597ce6145
hbonilla96 <hbonillab@ucenfotec.ac.cr>
2020-02-28 09:13:27 -0600

    Update userService.jsx

--- src/services/userService.jsx
+++ src/services/userService.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const user = data => {
-  const authEndpoint = "http://localhost:3378/api/v1/user";
+  const authEndpoint = "https://agnatebankapi.herokuapp.com/api/v1/user";
   const token = sessionStorage.getItem("token");
   return axios.post(authEndpoint, data);
 };
 
 export const changePassword = data => {
   const token = sessionStorage.getItem("token");
-  const passwordUrl = "http://localhost:3378/api/v1/user/update-password";
+  const passwordUrl = "https://agnatebankapi.herokuapp.com/api/v1/user/update-password";
   return axios.put(passwordUrl, data, {
     headers: { Authorization: "Bearer " + token }
   });
